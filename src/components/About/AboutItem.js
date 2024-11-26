a522609ca89043bc4cf19dd0a024bb166ba377fe
motaz99 <motaz.dn.ali@gmail.com>
2024-11-26 23:10:55 +0300

    refactor: using nextjs Image instead of html img

--- src/components/About/AboutItem.js
+++ src/components/About/AboutItem.js
@@ -1,19 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 const AboutItem = ({ id, image, icon, title, description }) => {
   return (
     <div className="bg-white transform transition duration-700 hover:scale-105 p-6 rounded-2xl hover:shadow-xl">
-      {/* image  */}
+      {/* image */}
       <div className="overflow-hidden rounded-2xl flex flex-grow">
-        <img
+        <Image
           className="transform transition duration-700 hover:scale-125"
           src={image}
           alt={title}
+          width={500}
+          height={300}
+          layout="responsive"
         />
       </div>
-      {/* other info  */}
-      <div className="flex mt-6 space-x-3 ">
-        {/* description  */}
+      {/* other info */}
+      <div className="flex mt-6 space-x-3">
+        {/* description */}
         <div className="flex flex-col space-y-3">
           <h1 className="text-xl text-gray-800 poppins">{title}</h1>
           <p className="text-sm text-gray-500 poppins">{description}</p>
