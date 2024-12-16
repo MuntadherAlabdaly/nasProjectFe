dd7c0a1ae8226ee38b034f9fd10107202036efbc
motaz99 <motaz.dn.ali@gmail.com>
2024-12-16 20:39:25 +0300

    refactor: make the banner responsive

--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -1,29 +1,37 @@
 import React from "react";
+import Image from "next/image";
+import Logo2 from "../../assets/logo2.png";
+
 import CameraStream from "../CameraStream";
 import { AiOutlineEye } from "react-icons/ai";
 
 const Banner = () => {
   return (
-    <section className="header-banner h-96 w-full">
-      <div className="flex flex-col items-center justify-center h-full">
-        <h1 className="text-center text-3xl md:text-4xl lg:text-5xl poppins font-semibold mt-16 sm:mt-9">
-          🍽️ عيونك على اكلك وهو يجهز
-        </h1>
-
-        <div className="p-6 rounded-xl w-full max-w-screen-lg h-[90vh] flex flex-col mt-12 sm:mt-0">
-          <div className="absolute top-2 left-2 flex items-center justify-center text-white bg-orange-500 rounded-lg px-5 py-2 text-lg font-bold">
-            مباشر
-          </div>
-
-          <div className="absolute top-2 right-2 flex items-center space-x-3 text-gray-200 bg-gray-800 bg-opacity-50 border border-gray-300 rounded-lg px-5 py-2">
-            <AiOutlineEye className="h-8 w-8 text-white" />
-            <span className="text-xl font-bold text-white">5</span>
-          </div>
-
-          <div className="relative overflow-hidden flex-1 flex items-center justify-center">
-            <CameraStream />
-          </div>
+    <section className="header-banner  w-full">
+      <div className="p-6 sm:pt-6 flex items-center justify-between">
+        <div className="absolute top-2 left-2 flex items-center justify-center text-white bg-orange-500 rounded-lg px-5 py-2 text-lg font-bold">
+          مباشر
         </div>
+
+        <div className="absolute top-2 right-2 flex items-center space-x-3 text-gray-200 bg-gray-800 bg-opacity-50 border border-gray-300 rounded-lg px-5 py-2">
+          <AiOutlineEye className="h-8 w-8 text-white" />
+          <span className="text-xl font-bold text-white">5</span>
+        </div>
+      </div>
+      <div className="mr-2">
+        <h1 className="text-center text-orange-500 text-xl md:text-4xl lg:text-4xl font-semibold mt-8 sm:mt-6 flex items-center justify-center">
+          <Image
+            className="mr-5"
+            src={Logo2}
+            alt="Logo"
+            width={80}
+            height={80}
+          />{" "}
+          عيونك على اكلك وهو يجهز
+        </h1>
+      </div>
+      <div className="overflow-hidden h-96 flex items-center justify-center p-4 pb-8">
+        <CameraStream />
       </div>
     </section>
   );
